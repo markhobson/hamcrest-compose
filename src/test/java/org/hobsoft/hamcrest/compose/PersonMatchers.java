03883b6bdc6fde089d8884e0b47f4e36ca8bbed0
Mark Hobson <markhobson@gmail.com>
2015-05-03 10:54:10 +0100

    Used hamcrest-core instead of library

--- src/test/java/org/hobsoft/hamcrest/compose/PersonMatchers.java
+++ src/test/java/org/hobsoft/hamcrest/compose/PersonMatchers.java
@@ -15,7 +15,7 @@ package org.hobsoft.hamcrest.compose;
 
 import org.hamcrest.Matcher;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hobsoft.hamcrest.compose.ConjunctionMatcher.compose;
 import static org.hobsoft.hamcrest.compose.HasFeatureMatcher.hasFeature;
 
