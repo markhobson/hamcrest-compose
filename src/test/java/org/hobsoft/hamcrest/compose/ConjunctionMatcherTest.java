ee3523aa5aff73a7e0032ef89e513fb2189342ae
Mark Hobson <markhobson@gmail.com>
2015-05-03 14:57:45 +0100

    Added ConjunctionMatcherTest

---
+++ src/test/java/org/hobsoft/hamcrest/compose/ConjunctionMatcherTest.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.hamcrest.compose;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests {@code ConjunctionMatcher}.
+ */
+public class ConjunctionMatcherTest
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// tests
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Test
+	public void describeWhenMatcherDescribesMatcher()
+	{
+		StringDescription description = new StringDescription();
+		
+		new ConjunctionMatcher<>(asList(anything("x"))).describeTo(description);
+		
+		assertThat(description.toString(), is("x"));
+	}
+	
+	@Test
+	public void describeWhenMatchersDescribesMatchers()
+	{
+		StringDescription description = new StringDescription();
+		
+		new ConjunctionMatcher<>(asList(anything("x"), anything("y"))).describeTo(description);
+		
+		assertThat(description.toString(), is("x and y"));
+	}
+	
+	@Test
+	public void matchesWhenMatchersMatchReturnsTrue()
+	{
+		assertThat(new ConjunctionMatcher<>(asList(startsWith("x"), endsWith("y"))).matches("xy"), is(true));
+	}
+	
+	@Test
+	public void matchesWhenFirstMatcherDoesNotMatchReturnsFalse()
+	{
+		assertThat(new ConjunctionMatcher<>(asList(startsWith("x"), endsWith("y"))).matches("zy"), is(false));
+	}
+	
+	@Test
+	public void matchesWhenSecondMatcherDoesNotMatchReturnsFalse()
+	{
+		assertThat(new ConjunctionMatcher<>(asList(startsWith("x"), endsWith("y"))).matches("xz"), is(false));
+	}
+	
+	@Test
+	public void matchesWhenMatchersDoNotMatchReturnsFalse()
+	{
+		assertThat(new ConjunctionMatcher<>(asList(startsWith("x"), endsWith("y"))).matches("z"), is(false));
+	}
+	
+	@Test
+	public void describeMismatchWhenFirstMatcherDoesNotMatchDescribesMismatch()
+	{
+		StringDescription description = new StringDescription();
+		
+		new ConjunctionMatcher<>(asList(nothing("x"), anything())).describeMismatch("y", description);
+		
+		assertThat(description.toString(), is("x was \"y\""));
+	}
+	
+	@Test
+	public void describeMismatchWhenSecondMatcherDoesNotMatchDescribesMismatch()
+	{
+		StringDescription description = new StringDescription();
+		
+		new ConjunctionMatcher<>(asList(anything(), nothing("x"))).describeMismatch("y", description);
+		
+		assertThat(description.toString(), is("x was \"y\""));
+	}
+	
+	@Test
+	public void describeMismatchWhenMatchersDoNotMatchDescribesMismatches()
+	{
+		StringDescription description = new StringDescription();
+		
+		new ConjunctionMatcher<>(asList(nothing("x"), nothing("y"))).describeMismatch("z", description);
+		
+		assertThat(description.toString(), is("x was \"z\" and y was \"z\""));
+	}
+	
+	// ----------------------------------------------------------------------------------------------------------------
+	// private methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private static <T> Matcher<T> nothing(String mismatch)
+	{
+		return new BaseMatcher<T>()
+		{
+			@Override
+			public void describeTo(Description description)
+			{
+				description.appendText("nothing");
+			}
+			
+			@Override
+			public boolean matches(Object actual)
+			{
+				return false;
+			}
+			
+			@Override
+			public void describeMismatch(Object actual, Description description)
+			{
+				description.appendText(mismatch).appendText(" was ").appendValue(actual);
+			}
+		};
+	}
+}
